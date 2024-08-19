f82262e06bb996a27d3afb1edd12b2e0bae5ebc1
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-08-19 20:55:19 +0300

    projects

--- lib/screens/portfolio/components/works_widget.dart
+++ lib/screens/portfolio/components/works_widget.dart
@@ -11,29 +11,39 @@ class WorksWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Slide> projects = [
       Slide(
-          image: 'images/custom_solution.jpg',
-          title: 'Project 1',
-          subtitle: 'Description 1'),
+          image: 'images/gympol-mockup.png',
+          title: 'GymPol',
+          subtitle:
+              'GymPol is a fitness app designed to simplify gym routines and track workouts, enhancing your bodybuilding experience efficiently.'),
       Slide(
-          image: 'images/cross_platform.webp',
-          title: 'Project 2',
-          subtitle: 'Description 2'),
+          image: 'images/gucumuzsensin.png',
+          title: 'Gücümüz Sensin',
+          subtitle:
+              'Gücümüz Sensin is an employee management app that allows your employees to reach out HR related information and perform various operations.'),
       Slide(
-          image: 'images/uiuxdesign.jpg',
-          title: 'Project 3',
-          subtitle: 'Description 3'),
+          image: 'images/miss.png',
+          title: 'Miss',
+          subtitle:
+              'Miss is an in-store sales tracking app that enables employees to manage inventory and perform various retail operations efficiently.'),
       Slide(
-          image: 'images/cross_platform.webp',
-          title: 'Project 4',
-          subtitle: 'Description 4'),
+          image: 'images/ama.png',
+          title: 'Akıllı Mağaza Asistanı',
+          subtitle:
+              'Akıllı Mağaza Asistanı is an employee management app that allows you to track employee performance and manage your workforce effectively.'),
       Slide(
-          image: 'images/custom_solution.jpg',
-          title: 'Project 5',
-          subtitle: 'Description 5'),
+          image: 'images/katil.png',
+          title: 'Katıl Mobil',
+          subtitle:
+              "Katıl Mobil is a social media app for creating and participating in surveys and polls, allowing you to earn money."),
       Slide(
           image: 'images/uiuxdesign.jpg',
           title: 'Project 6',
           subtitle: 'Description 6'),
+      Slide(
+          image: 'images/uiuxdesign.jpg',
+          title: '10+ More Projects',
+          subtitle:
+              'More projects are also created by request and for clients.'),
     ];
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -113,6 +123,79 @@ class WorksWidget extends StatelessWidget {
                     crossAxisCount: 3,
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(projects[index].image),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          const Spacer(),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 30, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Constants.primaryColor.withOpacity(0.9),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text(
+                                  projects[index].title,
+                                  style: AppTextStyles.w800_20
+                                      .copyWith(color: Constants.accentColor),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  projects[index].subtitle,
+                                  style: AppTextStyles.w400_16,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        if (!Responsive.isDesktop(context))
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Works',
+                  style: AppTextStyles.w400_40
+                      .copyWith(color: Constants.accentColor),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Explore my previous projects',
+                  style: AppTextStyles.w400_20,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: projects.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
                   ),
                   itemBuilder: (context, index) {
                     return Container(
