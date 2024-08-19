4de1772787b4d3d39e0686b8a1ef909d95a618ab
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-08-19 16:08:15 +0300

    mobile-view

--- lib/screens/portfolio/components/flutter_magic_widget.dart
+++ lib/screens/portfolio/components/flutter_magic_widget.dart
@@ -9,7 +9,7 @@ class FlutterMagicWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
-      if (Responsive.isDesktop(context)) {
+      if (!Responsive.isMobile(context)) {
         return SizedBox(
           height: 800,
           child: Row(
@@ -63,16 +63,57 @@ class FlutterMagicWidget extends StatelessWidget {
           ),
         );
       } else {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(),
-            Text(
-              "Crafting Flutter | iOS Magic",
-              style:
-                  AppTextStyles.w400_40.copyWith(color: Constants.accentColor),
-            ),
-          ],
+        return SizedBox(
+          height: 800,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Image.asset(
+                  "images/flutter_magic.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Crafting Flutter | iOS Magic",
+                        style: AppTextStyles.w400_20
+                            .copyWith(color: Constants.accentColor),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Transforming ideas into apps",
+                        style: AppTextStyles.w800_16,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "With 5 years of hands-on experience in Flutter, I craft robust, scalable mobile applications using state-of-the-art technologies like BLoC, Provider and cloud solutions such as Firebase and Supabase besides of backend integrations with REST APIs and GraphQL.",
+                        style: AppTextStyles.w400_14,
+                      ),
+                      const Text(
+                        "Also I have a strong background in iOS development with Swift and UIKit, SwiftUI.",
+                        style: AppTextStyles.w400_14,
+                      ),
+                      const Text(
+                        "Furthermore, I have experience in Node.js and Express.js for backend development.",
+                        style: AppTextStyles.w400_14,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       }
     });
